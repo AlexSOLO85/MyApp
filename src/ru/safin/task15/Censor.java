bba19b77aa3e9d2f84f774a676c898685d3eb9cb
AlexSOLO <dvjsalex@gmail.com>
2020-04-09 09:03:30 +0500

    Homework 11

---
+++ src/ru/safin/task15/Censor.java
@@ -0,0 +1,18 @@
+/*Напишите программу которая получает на вход некую строку , после она вызывает метод,
+заменяющий в строке все вхождения слова «бяка» на «вырезано цензурой» и выводит результат в консоль!
+ */
+
+package ru.safin.task15;
+
+import java.util.Scanner;
+
+class Censor {
+    public static void main(String[] args) {
+        String name;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Введите предложение или слово");
+        name = sc.nextLine();
+        String nameCensor = name.replaceAll("бяка", "*вырезано цензурой*");
+        System.out.println(nameCensor);
+    }
+}
