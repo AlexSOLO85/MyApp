6c37c9c9555996c097d1f856e55a31765a33dd39
AlexSOLO <dvjsalex@gmail.com>
2020-04-20 16:14:50 +0500

    Homework 15

---
+++ src/ru/safin/task19/Task19.java
@@ -0,0 +1,38 @@
+package ru.safin.task19;
+
+import java.io.File;
+import java.util.Objects;
+import java.util.Scanner;
+
+public class Task19 {
+
+    static String counter = " ";
+
+    public static void main(String[] args) {
+        System.out.println("Введите путь к папке: ");
+        Scanner sc = new Scanner(System.in);
+        String path = sc.nextLine();
+        File dir = new File(path);
+        if (!dir.exists()) {
+            System.out.println("Нет такой папки");
+        }
+        else  {
+            printAllFiles(dir);
+        }
+    }
+
+    static void printAllFiles(File dir) {
+        for (File file : Objects.requireNonNull(dir.listFiles())) {
+            print(file);
+            if (file.isDirectory()) {
+                counter += " ";
+                printAllFiles(file);
+            }
+        }
+        counter = " ";
+    }
+
+    static void print(File file) {
+        System.out.println(counter + File.separator + file.getAbsolutePath());
+    }
+}
