3e583d8cdedd309b8f5d9655ed4b6aefad4a3762
AlexSOLO <dvjsalex@gmail.com>
2020-05-14 22:58:47 +0500

    Preliminary Homework 23

---
+++ src/ru/safin/task29/Product.java
@@ -0,0 +1,27 @@
+package ru.safin.task29;
+
+public class Product {
+    private String name;
+    private int count;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setCount(int count) {
+        this.count = count;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCount() {
+        return count;
+    }
+
+    @Override
+    public String toString() {
+        return "Продукт: " + name + "\t | Количество: " + count;
+    }
+}
---
+++ src/ru/safin/task29/Store.java
@@ -0,0 +1,86 @@
+package ru.safin.task29;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Store implements Basket {
+
+    private final List<Product> products = new LinkedList<>();
+
+    public static void main(String[] args) {
+
+        Store stores = new Store();
+
+        stores.addProduct("Вода", 10);
+        stores.addProduct("Молоко", 5);
+        stores.addProduct("Кофе", 7);
+        stores.addProduct("Хлеб", 2);
+        showStore("Добавили товары в корзину", stores.products);
+
+        stores.removeProduct("");
+        showStore("Удалили товар из корзины", stores.products);
+
+        stores.updateProductQuantity("Чай", 10);
+        showStore("Обновили товар в корзине", stores.products);
+
+        stores.getProducts();
+
+        stores.clear();
+        System.out.println(stores.products.size());
+        showStore("Очистили товары в корзине", stores.products);
+    }
+
+    private static void showStore(String title, List<Product> products) {
+        System.out.println(title);
+        for (Object product : products) {
+            System.out.println(product);
+        }
+        System.out.println();
+    }
+
+    @Override
+    public void addProduct(String product, int quantity) {
+        Product prod = new Product();
+        prod.setName(product);
+        prod.setCount(quantity);
+        products.add(prod);
+    }
+
+    @Override
+    public void removeProduct(String product) {
+        Product prod = new Product();
+        prod.setName(product);
+        products.remove(0);
+    }
+
+    @Override
+    public void updateProductQuantity(String product, int quantity) {
+        Product prod = new Product();
+        prod.setName(product);
+        prod.setCount(quantity);
+        products.set(1, prod);
+    }
+
+    @Override
+    public void clear() {
+        products.clear();
+    }
+
+    @Override
+    public List<String> getProducts() {
+        List<String> stringList = new ArrayList<>();
+        stringList.add(products.toString());
+        System.out.println("Товары в корзине");
+        for (Product product : products) {
+            System.out.println(product);
+        }
+        System.out.println();
+        return stringList;
+    }
+
+    @Override
+    public int getProductQuantity(String product) {
+        return 0;
+    }
+}
