0d91f2c557297505dc8c4775ecd5fb333d355fcc
AlexSOLO <dvjsalex@gmail.com>
2020-06-03 20:29:37 +0500

    Homework 31

---
+++ src/main/java/ru/safin/homework31/Palindrome.java
@@ -0,0 +1,39 @@
+/*
+  Палиндром - это слово, фраза, число или другая последовательность символов или элементов,
+  которая читает то же самое вперед или назад.
+  Например: 12121 - палиндром, так как он читает то же самое вперед или назад. Мадам тоже палиндром.
+  Требуется написать 2 реализации проверки строки на палиндром:
+  1. Перемещение с обоих концов строки одновременно
+  2. Используя стандартные функции строки
+*/
+
+package ru.safin.homework31;
+
+import java.util.Scanner;
+
+public class Palindrome {
+
+    public static void main(String[] args) {
+        System.out.println("Проверка на палиндром: \"true\" - палиндром, \"false\" - не палиндром");
+        System.out.println("Введите слово для проверки:");
+        Scanner sc = new Scanner(System.in);
+        String str = sc.nextLine();
+        System.out.printf("%s %s\n", "1. Перемещение с обоих концов строки одновременно:", palindromeStr(str));
+        System.out.printf("%s %s\n", "2. Используя стандартные функции строки:", palindromeStd(str));
+    }
+
+    private static boolean palindromeStr(String text) {
+        for (int i = 0; i < text.length() / 2; i++) {
+            if (text.charAt(i) != text.charAt(text.length() - i - 1)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private static boolean palindromeStd(String text) {
+        StringBuilder stringBuilder = new StringBuilder(text);
+        stringBuilder.reverse();
+        return stringBuilder.toString().equals(text);
+    }
+}
